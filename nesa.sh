98231a62cba8e28395040b601afadf37c1911335
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-10-29 20:21:18 +0300

    Create nesa.sh

---
+++ nesa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Обновление и установка jq
+sudo apt update && sudo apt upgrade -y
+sudo apt install jq -y
+
+# Установка Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Установка Docker Compose
+VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Разрешение порта 31333
+sudo ufw allow 31333
+
+# Запуск bootstrap.sh
+bash <(curl -s https://raw.githubusercontent.com/nesaorg/bootstrap/master/bootstrap.sh)
